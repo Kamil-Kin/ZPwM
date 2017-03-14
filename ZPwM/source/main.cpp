877c4441ea6d04f8c4fdb25440802e98f9731724
Kamil Kin <kacper.kin28@gmail.com>
2017-03-14 20:53:41 +0100

    Zmiana

--- ZPwM/source/main.cpp
+++ ZPwM/source/main.cpp
@@ -2,6 +2,16 @@
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) 
 {
+  int iRetKey = MessageBox(0, "Podoba siê?", "app", MB_YESNO);
+  if (iRetKey == IDYES) 
+  {
+    MessageBox(0, "To dobrze", "App", MB_OK);
+  }
+  else 
+  {
+    MessageBox(0, "To Ÿle", "App2", MB_OK|MB_ICONQUESTION);
+  }
+
   MessageBox(0, "Hello World", "Okno", MB_OK);
   return 0;
 }
