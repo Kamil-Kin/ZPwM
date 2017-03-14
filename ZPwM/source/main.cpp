6bcec28b51ab87dcc73bc17e66d40e948bcf2bc3
Kamil Kin <kacper.kin28@gmail.com>
2017-03-14 20:35:25 +0100

    LAb 01

---
+++ ZPwM/source/main.cpp
@@ -0,0 +1,7 @@
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) 
+{
+  MessageBox(0, "Hello World", "Okno", MB_OK);
+  return 0;
+}
