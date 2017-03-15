26745f7b871e8bd917b0c53b437b5cabb5f3d7c4
Kamil Kin <kacper.kin28@gmail.com>
2017-03-15 22:14:37 +0100

    Zadanie domowe

--- ZPwM/source/main.cpp
+++ ZPwM/source/main.cpp
@@ -1,17 +1,30 @@
 #include <Windows.h>
+#include <stdio.h>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) 
 {
-  int iRetKey = MessageBox(0, "Podoba siê?", "app", MB_YESNO);
-  if (iRetKey == IDYES) 
-  {
-    MessageBox(0, "To dobrze", "App", MB_OK);
-  }
-  else 
-  {
-    MessageBox(0, "To Ÿle", "App2", MB_OK|MB_ICONQUESTION);
-  }
+  int i = MessageBox(0, "Wybierz liczbê od 1 do 40", "Start", MB_OK);
+  int max = 40;
+  int min = 1;
+  int mid = (max + min) / 2;
 
-  MessageBox(0, "Hello World", "Okno", MB_OK);
+  char tab[100];
+
+  while (max > min) 
+  {
+    sprintf_s(tab, "Czy Twoja liczba jest wiêksza od %d", mid);
+    i = MessageBox(0, tab, "Zgadywanka", MB_YESNO);
+    if (i == IDYES) 
+    {
+      min = mid + 1;
+    }
+    else 
+    {
+      max = mid;
+    }
+    mid = (max + min) / 2;
+  }
+  sprintf_s(tab, "Czy Twoja liczba to %d", min);
+  MessageBox(0,tab,"Koniec", MB_OK);
   return 0;
 }
