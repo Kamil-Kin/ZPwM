b1ac03b66a1bf7e037859c2dcb8f9ca2a0526563
Kamil-Kin <kacper.kin28@gmail.com>
2017-06-01 21:41:07 +0200

    kik bitmapy gotowe 1x1

--- Zajęcia 10.04/source/main.cpp
+++ Zajęcia 10.04/source/main.cpp
@@ -8,14 +8,43 @@ bool isFieldOccupiedBySecondPlayer[3][3];
 bool isDraw = false;  //czy remis
 int lRuch;  //liczba wykonanych ruch體
 
+int a = 0;
+int tab[3][3] = {   { 234, 45, 124 },
+                    { 547, 67, 34 },
+                    { 678, 768, 435 } };
+
+
 HBITMAP hBitmapGameBoard;
 HBITMAP hBitmapX;
 HBITMAP hBitmapO;
 HBITMAP hBitmapStart;
 HBITMAP hBitmapReset;
 HINSTANCE hInst;
-
-void DrawBitmap(HDC hdc) 
+bool CheckWinningCondition()
+{
+  if (
+    //sprawdzenie poziomo
+    tab[0][0] == tab[0][1] && tab[0][1] == tab[0][2] ||
+    tab[1][0] == tab[1][1] && tab[1][1] == tab[1][2] ||
+    tab[2][0] == tab[1][1] && tab[1][1] == tab[2][2] ||
+    //sprawdzenie pionowo
+    tab[0][0] == tab[1][0] && tab[1][0] == tab[2][0] ||
+    tab[0][1] == tab[1][1] && tab[1][1] == tab[2][1] ||
+    tab[0][2] == tab[1][2] && tab[1][2] == tab[2][2] ||
+    //sprawdzenie na ukos
+    tab[0][0] == tab[1][1] && tab[1][1] == tab[2][2] ||
+    tab[0][2] == tab[1][1] && tab[1][1] == tab[2][0]
+    )
+    return true;
+  else
+  {
+    lRuch++;
+    if (lRuch == 9) isDraw = true;
+    return false;
+  }
+  //return false;
+}
+void DrawBitmap(HDC hdc)
 {
   HDC hDCBitmap;
   hDCBitmap = CreateCompatibleDC(hdc);
@@ -23,8 +52,7 @@ void DrawBitmap(HDC hdc)
   BitBlt(hdc, 0, 0, 236, 217, hDCBitmap, 0, 0, SRCCOPY);
   DeleteDC(hDCBitmap);
 }
-
-void DrawX(HDC hdc, int x, int y) 
+void DrawX(HDC hdc, int x, int y)
 {
   HDC hDCBitmap;
   hDCBitmap = CreateCompatibleDC(hdc);
@@ -32,8 +60,7 @@ void DrawX(HDC hdc, int x, int y)
   BitBlt(hdc, x, y, 67, 75, hDCBitmap, 0, 0, SRCCOPY);
   DeleteDC(hDCBitmap);
 }
-
-void DrawO(HDC hdc, int x, int y) 
+void DrawO(HDC hdc, int x, int y)
 {
   HDC hDCBitmap;
   hDCBitmap = CreateCompatibleDC(hdc);
@@ -42,9 +69,9 @@ void DrawO(HDC hdc, int x, int y)
   DeleteDC(hDCBitmap);
 }
 
-LRESULT CALLBACK ButtonWndProc( HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK ButtonWndProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-  switch (uMsg) 
+  switch (uMsg)
   {
   case WM_PAINT:
   {
@@ -57,194 +84,210 @@ LRESULT CALLBACK ButtonWndProc( HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM l
     DeleteDC(hDCBitmap);*/
     ReleaseDC(hwndDlg, hdc);
   }
-    return 0;
+  return 0;
   default:
     return DefWindowProc(hwndDlg, uMsg, wParam, lParam);
     //return CallWindowProc(wpOrgButtonProc, hwnd, uMsg, wParam, lParam);
   }
 }
 
-INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam) 
+INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
   switch (uMsg)
   {
-  case WM_INITDIALOG:
-  {
-    hBitmapGameBoard = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_PLANSZA));
-    hBitmapX = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_X));
-    hBitmapO = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_O));
-    hBitmapStart = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_START));
-    hBitmapReset = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_RESET));
+    case WM_INITDIALOG:
+    {
+      hBitmapGameBoard = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_PLANSZA));
+      hBitmapX = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_X));
+      hBitmapO = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_O));
+      hBitmapStart = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_START));
+      hBitmapReset = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_RESET));
 
-    HWND hwndButton = GetDlgItem(hwndDlg, IDC_BUTTON_START);
-    WNDPROC wpOrgButtonProc = (WNDPROC)SetWindowLong(hwndButton, -4, (LONG)ButtonWndProc);
-  }
+      HWND hwndButton = GetDlgItem(hwndDlg, IDC_BUTTON_START);
+      WNDPROC wpOrgButtonProc = (WNDPROC)SetWindowLong(hwndButton, -4, (LONG)ButtonWndProc);
+    }
     return TRUE;
 
-  case WM_COMMAND:
-  {
-    switch (HIWORD(wParam))
+    case WM_COMMAND:
     {
-    case BN_CLICKED:    //zdarzenie klikni阠ia
-      switch (LOWORD(wParam))
+      switch (HIWORD(wParam))
       {
-      case IDC_BUTTON_START:
-        if (isGameOn == false)
+      case BN_CLICKED:    //zdarzenie klikni阠ia
+        switch (LOWORD(wParam))
         {
-          isGameOn = true; CHAR szText[500];
-          /*HWND hwndStatic = GetDlgItem(hwndDlg, IDC_STATIC1);
-
-          wsprintf(szText, "Start");
-          SetWindowText(hwndStatic, szText);
-          for (int i = 0; i < 9; i++)
+        case IDC_BUTTON_START:
+          if (isGameOn == false)
           {
-          HWND hwndButton = GetDlgItem(hwndDlg, IDC_BUTTON1 + i);
-          CHAR szText[500];
-          wsprintf(szText, "");
-          SetWindowText(hwndButton, szText);
-          }*/
-          HWND hwndButtonStart = GetDlgItem(hwndDlg, IDC_BUTTON_START);
-          wsprintf(szText, "Stop");
-          SetWindowText(hwndButtonStart, szText);
+            isGameOn = true; 
+            a = 0;
+            for (int i = 0; i < 3; i++)
+              for (int j = 0; j < 3; j++)
+                tab[i][j] = a++;
+            isDraw = false;
+            lRuch = 0;
+            CheckRadioButton(hwndDlg, IDC_RADIO1, IDC_RADIO2, IDC_RADIO1);
+            CHAR szText[500];
+            HWND hwndStatic = GetDlgItem(hwndDlg, IDC_STATIC_SHOW);
+            wsprintf(szText, "Gra rozpocz阾a");
+            SetWindowText(hwndStatic, szText);
+            HWND hwndButtonStart = GetDlgItem(hwndDlg, IDC_BUTTON_START);
+            wsprintf(szText, "Stop");
+            SetWindowText(hwndButtonStart, szText);
 
-          //Rysowanie siatki gry
-          /*HDC hdc = GetDC(hwndDlg);
-          //LineTo
-          //MovetoEx
-          //Rectangle
-          //TextOut
-          //GetPixel
-          //SetPixel
-          HPEN hMyPen = CreatePen(PS_SOLID, 5, RGB(255, 255, 0));
-          SelectObject(hdc, hMyPen);
+            //Rysowanie siatki gry
+            HDC hdc = GetDC(hwndDlg);
+            DrawBitmap(hdc);
+            ReleaseDC(hwndDlg, hdc);
 
-          MoveToEx(hdc, 100, 30, NULL);
-          LineTo(hdc, 100, 230);
-          MoveToEx(hdc, 180, 30, NULL);
-          LineTo(hdc, 180, 230);
-          MoveToEx(hdc, 40, 100, NULL);
-          LineTo(hdc, 240, 100);
-          MoveToEx(hdc, 40, 180, NULL);
-          LineTo(hdc, 240, 180);
-          //DeleteObject(hMyPen);
-          TextOut(hdc, 0, 0, szText, strlen(szText));
-          ReleaseDC(hwndDlg, hdc);*/
-
-          for (int i = 0; i < 3; i++)
-          {
-            for (int j = 0; j < 3; j++)
+            for (int i = 0; i < 3; i++)
             {
-              isFieldOccupiedByFirstPlayer[i][j] = false;
-              isFieldOccupiedBySecondPlayer[i][j] = false;
+              for (int j = 0; j < 3; j++)
+              {
+                isFieldOccupiedByFirstPlayer[i][j] = false;
+                isFieldOccupiedBySecondPlayer[i][j] = false;
+              }
             }
-          }
-        }
-        else
-        {
-          isGameOn = false;
-          HWND hwndButtonStart = GetDlgItem(hwndDlg, IDC_BUTTON_START);
-          CHAR szText[500];
-          wsprintf(szText, "Start");
-          SetWindowText(hwndButtonStart, szText);
-        }
-      }
-      return TRUE;
-    }
-  }
-    return FALSE;
-
-  case WM_LBUTTONDOWN:
-    //CHAR szText[200];
-    if (isGameOn == true)
-    {
-      int x = LOWORD(lParam); int y = HIWORD(lParam);
-      if ((x > 0 && x < 236) && (y > 0 && y < 217))
-      {
-        int fieldX = (x - 0) / 78;
-        int fieldY = (y - 0) / 72;
-        if ((isFieldOccupiedByFirstPlayer[fieldX][fieldY] == false) &&
-          (isFieldOccupiedBySecondPlayer[fieldX][fieldY] == false))
-        {
-          x = fieldX * 78 + 39;
-          y = fieldY * 72 + 36;
-          HDC hdc = GetDC(hwndDlg);
-          /*int iMinBoardX = 50;
-          int iWidthBoardX = 30;
-          int iMinBoardY = 60;
-          int iWidthBoardY = 30;
-          x = ((x - iMinBoardX) / iWidthBoardX)*iWidthBoardX + iMinBoardX + iWidthBoardX / 2;
-          y = ((y - iMinBoardY) / iWidthBoardY)*iWidthBoardY + iMinBoardY + iWidthBoardY / 2;
-          */
-          if (isFirstPlayerTurn == true)
-          {
-            DrawX(hdc, x - 34, y - 38);
-            isFieldOccupiedByFirstPlayer[fieldX][fieldY] = true;
           }
           else
           {
-            DrawO(hdc, x - 29, y - 27);
-            isFieldOccupiedBySecondPlayer[fieldX][fieldY] = true;
+            isGameOn = false;
+            HWND hwndStatic = GetDlgItem(hwndDlg, IDC_STATIC_SHOW);
+            HWND hwndButtonStart = GetDlgItem(hwndDlg, IDC_BUTTON_START);
+            CHAR szText[500];
+            wsprintf(szText, "Gra wstrzymana");
+            SetWindowText(hwndStatic, szText);
+            wsprintf(szText, "Start");
+            SetWindowText(hwndButtonStart, szText);
           }
-          ReleaseDC(hwndDlg, hdc);
-          isFirstPlayerTurn = !isFirstPlayerTurn; //prze彻czanie graczy
+        }
+        return TRUE;
+      }
+    }
+    return FALSE;
+
+    case WM_LBUTTONDOWN: {
+      //CHAR szText[200];
+      if (isGameOn == true)
+      {
+        int x = LOWORD(lParam); int y = HIWORD(lParam);
+        if ((x > 0 && x < 236) && (y > 0 && y < 217))
+        {
+          int fieldX = (x - 0) / 78;
+          int fieldY = (y - 0) / 72;
+          if ((isFieldOccupiedByFirstPlayer[fieldX][fieldY] == false) &&
+            (isFieldOccupiedBySecondPlayer[fieldX][fieldY] == false))
+          {
+            x = fieldX * 78 + 78 / 2;
+            y = fieldY * 72 + 72 / 2;
+            HDC hdc = GetDC(hwndDlg);
+            if (isFirstPlayerTurn == true)
+            {
+              DrawX(hdc, x - 34, y - 38);
+              isFieldOccupiedByFirstPlayer[fieldX][fieldY] = true;
+              tab[fieldX][fieldY] = 'X';
+              CheckRadioButton(hwndDlg, IDC_RADIO1, IDC_RADIO2, IDC_RADIO2);
+              if (CheckWinningCondition()) 
+              {
+                HWND hwndStatic = GetDlgItem(hwndDlg, IDC_STATIC_SHOW);
+                CHAR szText[500];
+                wsprintf(szText, "Wygrana X, aby zacz规 od nowa, wci渘ij start");
+                SetWindowText(hwndStatic, szText);
+                isGameOn = false;
+                HWND hwndButton_start = GetDlgItem(hwndDlg, IDC_BUTTON_START);
+                wsprintf(szText, "Start");
+                SetWindowText(hwndButton_start, szText);
+              }
+              else if (isDraw)
+              {
+                HWND hwndStatic = GetDlgItem(hwndDlg, IDC_STATIC_SHOW);
+                CHAR szText[500];
+                wsprintf(szText, "Remis,aby zacz规 od nowa, wci渘ij start");
+                SetWindowText(hwndStatic, szText);
+                isGameOn = false;
+                HWND hwndButton_start = GetDlgItem(hwndDlg, IDC_BUTTON_START);
+                wsprintf(szText, "Start");
+                SetWindowText(hwndButton_start, szText);
+              }
+            }
+            else
+            {
+              DrawO(hdc, x - 29, y - 27);
+              isFieldOccupiedBySecondPlayer[fieldX][fieldY] = true;
+              tab[fieldX][fieldY] = 'O';
+              CheckRadioButton(hwndDlg, IDC_RADIO1, IDC_RADIO2, IDC_RADIO1);
+              if (CheckWinningCondition())
+              {
+                HWND hwndStatic = GetDlgItem(hwndDlg, IDC_STATIC_SHOW);
+                CHAR szText[500];
+                wsprintf(szText, "Wygrana O, aby zacz规 od nowa, wci渘ij start");
+                SetWindowText(hwndStatic, szText);
+                isGameOn = false;
+                HWND hwndButton_start = GetDlgItem(hwndDlg, IDC_BUTTON_START);
+                wsprintf(szText, "Start");
+                SetWindowText(hwndButton_start, szText);
+              }
+              else if (isDraw)
+              {
+                HWND hwndStatic = GetDlgItem(hwndDlg, IDC_STATIC_SHOW);
+                CHAR szText[500];
+                wsprintf(szText, "Remis,aby zacz规 od nowa, wci渘ij start");
+                SetWindowText(hwndStatic, szText);
+                isGameOn = false;
+                HWND hwndButton_start = GetDlgItem(hwndDlg, IDC_BUTTON_START);
+                wsprintf(szText, "Start");
+                SetWindowText(hwndButton_start, szText);
+              }
+            }
+            ReleaseDC(hwndDlg, hdc);
+            isFirstPlayerTurn = !isFirstPlayerTurn; //prze彻czanie graczy
+          }
         }
       }
     }
-    return TRUE;
+      return TRUE;
 
-  case WM_PAINT:
-  {
-     HDC hdc = GetDC(hwndDlg);
-    //BitBlt(hdc, 0, 0, 1920, 1080, GetDC(0), 0, 0, SRCCOPY); // kopiowanie zawarto渃i kontekstu graficznego do drugiego
-    //DrawBoard(hdc);
-    //HPEN hMyPen = CreatePen(PS_SOLID, 5, RGB(255, 255, 0));
-    //SelectObject(hdc, hMyPen);
-
-    /*MoveToEx(hdc, 100, 30, NULL);
-    LineTo(hdc, 100, 230);
-    MoveToEx(hdc, 180, 30, NULL);
-    LineTo(hdc, 180, 230);
-    MoveToEx(hdc, 40, 100, NULL);
-    LineTo(hdc, 240, 100);
-    MoveToEx(hdc, 40, 180, NULL);
-    LineTo(hdc, 240, 180);*/
-    //DrawAllBoards(hdc);
-    /*for (int iFieldX = 0; iFieldX < 3; iFieldX++)
+    case WM_PAINT:
     {
-      for (int iFieldY = 0; iFieldY < 3; iFieldY++)
+      HDC hdc = GetDC(hwndDlg);
+      DrawBitmap(hdc);
+  /*
+      for (int iFieldX = 0; iFieldX < 3; iFieldX++)
       {
-        if (isFieldOccupiedByFirstPlayer[iFieldX][iFieldY] == true)
+        for (int iFieldY = 0; iFieldY < 3; iFieldY++)
         {
-          DrawX(hdc, iFieldX, iFieldY);
+          if (isFieldOccupiedByFirstPlayer[iFieldX][iFieldY] == true)
+          {
+            DrawX(hdc, iFieldX, iFieldY);
+          }
+          if (isFieldOccupiedBySecondPlayer[iFieldX][iFieldY] == true)
+          {
+            DrawO(hdc, iFieldX, iFieldY);
+          }
         }
-        if (isFieldOccupiedBySecondPlayer[iFieldX][iFieldY] == true)
-        {
-          DrawO(hdc, iFieldX, iFieldY);
-        }
-      }
-    }*/
-
-    //DrawBitmap(hdc);
-    //ReleaseDC(hwndDlg, hdc);
-  }
+      }*/
+      ReleaseDC(hwndDlg, hdc);
+      return DefWindowProc(hwndDlg, uMsg, wParam, lParam);
+    }
     return TRUE;
 
-  case WM_CLOSE:    //komunikat zamkni阠ia okienka
-    DestroyWindow(hwndDlg);
-    PostQuitMessage(0);
-    for (int i = 0; i < 5; i++) { DeleteObject(hBitmapGameBoard+i); }
-    return TRUE;
+    case WM_CLOSE: //komunikat zamkni阠ia okienka
+    {
+      DestroyWindow(hwndDlg);
+      PostQuitMessage(0);
+      for (int i = 0; i < 5; i++) { DeleteObject(hBitmapGameBoard + i); }
+      return TRUE;
+    }
   }
   return FALSE;
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) 
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
 {
   hInst = hInstance;
   HWND hwndMainWindow = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_MAINVIEW), NULL, DialogProc);
-  ShowWindow(hwndMainWindow, iCmdShow); 
+  ShowWindow(hwndMainWindow, iCmdShow);
   MSG msg = {};
-  while (GetMessage(&msg, NULL, 0, 0)) 
+  while (GetMessage(&msg, NULL, 0, 0))
   {
     TranslateMessage(&msg);
     DispatchMessage(&msg);
